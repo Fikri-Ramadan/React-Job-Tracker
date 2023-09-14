221e74b7862fb45d8ca299a10de8eec5e4b62e23
Fikri-Ramadan <fikrinr.tech@gmail.com>
2023-09-14 12:19:39 +0700

    change axios baseURL to localhost

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const customFetch = axios.create({
-  baseURL: 'https://job-tracker-api.vercel.app/api/v1',
+  // baseURL: 'https://job-tracker-api.vercel.app/api/v1',
+  baseURL: 'http://localhost:3000/api/v1',
   withCredentials: true,
 });
 
