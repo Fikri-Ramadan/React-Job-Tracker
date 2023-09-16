8a8ad5762ac193119845e9c528a85d66ded2de43
Fikri-Ramadan <fikrinr.tech@gmail.com>
2023-09-16 10:20:47 +0700

    change axios baseURL

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const customFetch = axios.create({
-  baseURL: 'https://fikri-jobtracker-api.vercel.app/api/v1',
+  // baseURL: 'https://fikri-jobtracker-api.vercel.app/api/v1',
+  baseURL: 'https://fikri-jobtracker.vercel.app/api/v1',
   // baseURL: 'http://localhost:3000/api/v1',
   withCredentials: true,
 });
